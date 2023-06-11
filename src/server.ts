7ae806bc99af87fce2d94d7e6dc73dedd7268971
Iarlen <iarlensr@hotmail.com>
2023-06-11 01:20:10 -0300

    feat: create class Server to initialize the app.

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import fastify, { FastifyInstance } from 'fastify'
+import { MenuRoutes } from './routes/menu'
+
+class Server {
+  app: FastifyInstance
+
+  constructor() {
+    this.app = fastify()
+  }
+
+  routes() {
+    const menuRoutes = new MenuRoutes(this.app)
+    menuRoutes.allMenuRoutes()
+  }
+
+  start() {
+    this.routes()
+
+    this.app
+      .listen({
+        port: 3333,
+        host: '0.0.0.0',
+      })
+      .then((response) => console.log('Server started on port 3333.'))
+  }
+}
+
+const server = new Server()
+
+server.start()
