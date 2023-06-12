ccfc3ccb3030f7c1e5f29405f47bfc93f183c017
Iarlen <iarlensr@hotmail.com>
2023-06-12 01:52:17 -0300

    refactor: adding authentication in the routes of creating, deleting and changing a dish.

--- src/routes/dishesRoutes.ts
+++ src/routes/dishesRoutes.ts
@@ -1,22 +1,39 @@
 import { FastifyInstance } from 'fastify'
 import { DishesController } from '../controllers/dishesController'
 
+import { AuthenticationMiddleware } from '../middleware/AuthenticationMiddleware'
+
 export class DishesRoutes {
   app: FastifyInstance
   dishesController: DishesController
+  auth: AuthenticationMiddleware
 
   constructor(app: FastifyInstance) {
     this.app = app
     this.dishesController = new DishesController()
+    this.auth = new AuthenticationMiddleware()
 
     this.allDishesRoutes()
   }
 
   allDishesRoutes() {
     this.app.get('/dishes', this.dishesController.index)
-    this.app.post('/dishes', this.dishesController.store)
     this.app.get('/dishes/:id', this.dishesController.show)
-    this.app.put('/dishes/:id', this.dishesController.update)
-    this.app.delete('/dishes/:id', this.dishesController.delete)
+
+    this.app.post(
+      '/dishes',
+      { preHandler: this.auth.authenticate },
+      this.dishesController.store,
+    )
+    this.app.put(
+      '/dishes/:id',
+      { preHandler: this.auth.authenticate },
+      this.dishesController.update,
+    )
+    this.app.delete(
+      '/dishes/:id',
+      { preHandler: this.auth.authenticate },
+      this.dishesController.delete,
+    )
   }
 }
