2b4ded0a29f5a1c4b9f3406c1bfa7c7c0a5b8b32
Iarlen <iarlensr@hotmail.com>
2023-06-12 01:51:12 -0300

    feat: creating authentication middleware.

---
+++ src/middleware/AuthenticationMiddleware.ts
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken'
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { env } from '../utils/envShema'
+
+interface TokenPayload {
+  sub: string
+  email: string
+  name: string
+}
+
+export class AuthenticationMiddleware {
+  async authenticate(request: FastifyRequest, reply: FastifyReply) {
+    const token = request.headers.authorization?.replace('Bearer ', '')
+
+    if (!token) {
+      return reply.status(401).send({ error: 'Token not provided.' })
+    }
+
+    try {
+      const decoded = jwt.verify(token, env.SECRET) as TokenPayload
+      request.user = decoded
+    } catch (err) {
+      reply.status(401).send({ error: 'Invalid token.' })
+    }
+  }
+}
