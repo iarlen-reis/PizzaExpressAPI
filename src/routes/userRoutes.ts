c4657952a8fe59cd30cb6a139c7e2f1c41987cd3
Iarlen <iarlensr@hotmail.com>
2023-06-12 17:25:14 -0300

    feat: creating profile route in userRoutes.

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,13 +1,16 @@
 import { FastifyInstance } from 'fastify'
 import { UserController } from '../controllers/userController'
+import { AuthenticationMiddleware } from '../middleware/AuthenticationMiddleware'
 
 export class UserRoutes {
   app: FastifyInstance
   userController: UserController
+  auth: AuthenticationMiddleware
 
   constructor(app: FastifyInstance) {
     this.app = app
     this.userController = new UserController(this.app)
+    this.auth = new AuthenticationMiddleware()
 
     this.allUserRoutes()
   }
@@ -15,5 +18,10 @@ export class UserRoutes {
   allUserRoutes() {
     this.app.post('/auth/register', this.userController.register)
     this.app.post('/auth/login', this.userController.login)
+    this.app.get(
+      '/auth/profile',
+      { preHandler: this.auth.authenticate },
+      this.userController.profile,
+    )
   }
 }
